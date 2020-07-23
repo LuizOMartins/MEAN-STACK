9f5c8d29a9a35987ee96db5e58986b3616f065bf
Luiz Eduardo Martins Martins <1690342@TCS.com>
2020-07-22 23:18:30 -0300

    Organizando o projeto

--- alurapic/app/routes/fotos.js
+++ alurapic/app/routes/fotos.js
@@ -1,21 +1,9 @@
+var api = require('../api/fotos');
+
 module.exports = function (app) {
 
 
-
-
-app.get('/v1/fotos',function (req,res){
-
-    var fotos = [
-        {_id: 1, titulo: 'Leão', url:'http://www.fundosanimais.com/Minis/leoes.jpg' },
-        {_id: 2, titulo: 'Leão 2', url:'http://www.fundosanimais.com/Minis/leoes.jpg' }
-    ];
-
-    res.json(fotos);
-
-});
-
-
-
+app.get('/v1/fotos',api.lista);
 
 
 }
