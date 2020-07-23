802f5da04dfccf15bf30dede861e9c7bf9a29519
Luiz Eduardo Martins Martins <1690342@TCS.com>
2020-07-22 22:40:20 -0300

    Carregando arquvos com o Consign

--- alurapic/config/express.js
+++ alurapic/config/express.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var app = express();
+var consign =  require('consign');
 
 // nosso primeiro middleware
 app.use(express.static('./public'));
 
 
-require('../app/routes/fotos')(app);
-require('../app/routes/grupo')(app);
+consign().include('app/routes').into(app);
+
+// require('../app/routes/fotos')(app);
+// require('../app/routes/grupo')(app);
 
 module.exports = app; // exportando nossa instância do Express
