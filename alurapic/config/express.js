ddb4067c2fbdc9757095edb731063ac3b6b19fc0
Luiz Eduardo Martins Martins <1690342@TCS.com>
2020-07-22 22:20:16 -0300

    Iniciando configurações de endpoint no Express

--- alurapic/config/express.js
+++ alurapic/config/express.js
@@ -4,4 +4,20 @@ var app = express();
 // nosso primeiro middleware
 app.use(express.static('./public'));
 
+
+app.get('/v1/fotos',function (req,res){
+
+    var fotos = [
+        {_id: 1, titulo: 'Leão', url:'http://www.fundosanimais.com/Minis/leoes.jpg' },
+        {_id: 2, titulo: 'Leão 2', url:'http://www.fundosanimais.com/Minis/leoes.jpg' }
+    ];
+
+    res.json(fotos);
+
+});
+
+
+
+
+
 module.exports = app; // exportando nossa instância do Express
