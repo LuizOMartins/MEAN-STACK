bf636b1ea7c306b0de6665b24019b400982fcb21
Luiz Eduardo Martins Martins <1690342@TCS.com>
2020-08-02 23:00:07 -0300

    CRUD realizado utilizando Mongoose

--- alurapic/app/api/fotos.js
+++ alurapic/app/api/fotos.js
@@ -2,9 +2,10 @@ var mongoose =  require('mongoose');
 
 
 var api = {};
+var model =  mongoose.model('Foto');
 
 api.lista = function (req,res){
-    var model =  mongoose.model('Foto');
+   
 
     //SINCRONO
     // model.find(function(error, fotos){ // busca todos
@@ -29,29 +30,60 @@ api.lista = function (req,res){
 
 api.buscaPorId =  function(req, res){
 
+model
+.findById(req.params.id)
+.then(function(foto){
+    
+    if(!foto) throw Error ('Foto não encontrada');
+    res.json(foto);
 
+}, function(error){
+    console.log(error);
+    res.status(404).json(error);
+});
 
 
 };
 
 
 api.removePorId =  function(req, res){
+    model
+    .remove({_id: req.params.id})
+    .then(function(){
+        res.sendStats(204);
+    }, function(error){
+        console.log(error);
+        res.status(500).json(error);
+    });
 
 };
 
 
 api.adiciona = function(req, res){
-    var foto = req.body;
-    foto._id = ++CONTADOR;
-    fotos.push(foto);
 
-    res.json(foto);
+    
+    model
+    .create(req.body)
+    .then(function(foto){
+        res.json(foto);
+
+    }),function(error){
+        console.log(error);
+        res.status(500).json(error);
+    }
 
 
 };
 
-api.atualiza = function(req,res){
+api.atualiza = function(req, res) {
 
+    model.findByIdAndUpdate(req.params.id, req.body)
+    .then(function(foto) {
+        res.json(foto);
+    }, function(error) {
+        console.log(error);
+        res.sendStatus(500);
+    })
 };
 
 
