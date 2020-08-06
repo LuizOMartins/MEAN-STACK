57b255fae70974d47e309eb92d0523bc78584264
Luiz Eduardo <43974104+LuizOMartins@users.noreply.github.com>
2020-08-05 21:44:06 -0300

    Criando Menu Lateral

---
+++ alurapic/public/js/efeitos.js
@@ -0,0 +1,12 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "15%";
+    document.getElementById("body").style.width = "85%";
+
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("body").style.width = "100%";
+  }
+
+  
